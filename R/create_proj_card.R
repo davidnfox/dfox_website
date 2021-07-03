b9bb5081d07322bc03ac5c26f0e6bd4c059dd5c3
David Fox <fox.david.n@gmail.com>
2021-07-03 09:37:25 -0400

    update project cards
    does this change?

--- R/create_proj_card.R
+++ R/create_proj_card.R
@@ -4,18 +4,18 @@
 
 library(htmltools)
 
-create_proj_card <- function(img_src, url, title, text, text2){
+create_proj_card = function(img_src, url, title, text, text2){
   tags$div(
     class = "card",
     tags$img(class="card-img-top",
              src=img_src,
-             style="width:400px;"),
+             style="width:300px;"),
     tags$div(
       class = "card-body",
       h4(
-        a(href = url,
+        a(href = url,  
           title)),
-      p(class = "card-text",
+      p(class = "card-text", 
         text, br(), text2)
     )
   )
